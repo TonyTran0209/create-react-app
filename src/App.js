65286be4f021cfe5ef4134b8c83e4f24838926fa
Tony Tran <phi.tran.0209@gmail.com>
2017-12-08 18:07:58 +0700

    8. Add Custom propType Validation to React Components https://egghead.io/lessons/react-custom-proptype-validation
    Custom Validation

--- src/App.js
+++ src/App.js
@@ -34,16 +34,22 @@ class App extends React.Component {
 
 class App extends React.Component {
     render() {
-        return <Button>I <Heart /> React</Button>
+        return <Title text="123" />
     }
 }
 
-class Heart extends React.Component {
-    render() {
-        return <span>&hearts;</span>
+const Title = (props) => <h1>Title: {props.text}</h1>
+
+Title.propTypes = {
+    // text: React.PropTypes.string.isRequired
+    text(props, propName, component){
+        if(!(propName in props)){
+            return new Error(`missing ${propName}`)
+        }
+        if(props[propName].length < 6){
+            return new Error(`${propName} was too short`)
+        }
     }
 }
-
-const Button = (props) => <button>{props.children}</button>
 
 export default App
