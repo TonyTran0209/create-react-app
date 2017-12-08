65c08f51af9af6bb7a1340f66e6d3feb7a4356cd
Tony Tran <phi.tran.0209@gmail.com>
2017-12-08 15:37:15 +0700

    3. Display Output in React with a Component's render Method https://egghead.io/lessons/react-the-render-method
    4. Set Properties on React Components
    https://egghead.io/lessons/react-introduction-to-properties

--- src/App.js
+++ src/App.js
@@ -17,6 +17,36 @@ class App extends React.Component {
 */
 
 // 3rd way
-const App = () => <h1>3rd Hello</h1>
+// const App = () => <h1>3rd Hello</h1>
+
+/* multi-lines
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <b>Hello</b>
+                <b>Tony</b>
+            </div>
+        )
+    }
+}
+*/
+
+class App extends React.Component {
+    render() {
+        let name = this.props.name
+        let txt_default = this.props.txt_default
+        return (
+            <p>
+                <h1>I'm {name}, {this.props.age} years old</h1>
+                <h2>{txt_default}</h2>
+            </p>
+        )
+    }
+}
+
+App.defaultProps = {
+    txt_default: "this is the default text"
+}
 
 export default App
