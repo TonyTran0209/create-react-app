333850e188ba87343d6b00b4497d7ee17cae05d5
Tony Tran <phi.tran.0209@gmail.com>
2017-12-11 10:37:15 +0700

    10.a. Use React ref to Get a Reference to Specific Components https://egghead.io/lessons/react-using-refs-to-access-components
    Binding specific fields

--- src/App.js
+++ src/App.js
@@ -3,29 +3,28 @@ import React from 'react';
 class App extends React.Component {
     constructor(){
         super();
-        this.state = {currentEvent: 'nothing'}
-        this.update = this.update.bind(this)
+        this.state = {a: '', b: ''}
     }
-    update(e){
-        this.setState({currentEvent: e.type})
+    update(){
+        this.setState({
+            a: this.refs.a.value,
+            b: this.refs.b.value
+        })
     }
     render(){
         return (
             <div>
-                <textarea
-                    onKeyPress={this.update}
-                    onCopy={this.update}
-                    onCut={this.update}
-                    onPaste={this.update}
-                    onFocus={this.update}
-                    onBlur={this.update}
-                    onDoubleClick={this.update}
-                    onTouchStart={this.update}
-                    onTouchMove={this.update}
-                    onTouchEnd={this.update}
-                    cols="30"
-                    rows="10" />
-                <h1>Current Event: {this.state.currentEvent}</h1>
+                <input
+                    ref="a"
+                    type="text"
+                    onChange={this.update.bind(this)}
+                /> {this.state.a}
+                <hr />
+                <input
+                    ref="b"
+                    type="text"
+                    onChange={this.update.bind(this)}
+                /> {this.state.b}
             </div>
         )
     }
