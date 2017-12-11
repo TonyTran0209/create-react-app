e6eadd430d9f8d256d6ae1075662da953ed5d496
Tony Tran <phi.tran.0209@gmail.com>
2017-12-11 15:45:18 +0700

    15.a. Compose React Component Behavior with Higher Order Components https://egghead.io/lessons/react-react-fundamentals-higher-order-components-replaces-mixins

--- src/App.js
+++ src/App.js
@@ -1,32 +1,25 @@
 import React from 'react';
 
 class App extends React.Component {
-    constructor(){
-        super();
-        this.state = {items: []}
-    }
-    componentWillMount(){
-        fetch( 'https://swapi.co/api/people/?format=json' )
-            .then( response => response.json() )
-            .then( ({results: items}) => this.setState({items}))
-    }
-    filter(e){
-        this.setState({filter: e.target.value})
-    }
     render(){
-        let items = this.state.items;
-        if(this.state.filter){
-            items = items.filter( item => item.name.toLowerCase().includes(this.state.filter.toLowerCase()))
-        }
         return (
             <div>
-                <input type="text" onChange={this.filter.bind(this)} placeholder="type to search..." />
-                {items.map(item => <Person key={item.name} person={item} />)}
+                <Button>button</Button>
+                <hr/>
+                <Label>label</Label>
             </div>
         )
     }
 }
 
-const Person = (props) => <h5>{props.person.name}</h5>
+const Button = (props) => <button>{props.children}</button>
+
+class Label extends React.Component {
+    render(){
+        return (
+            <label>{this.props.children}</label>
+        )
+    }
+}
 
 export default App
